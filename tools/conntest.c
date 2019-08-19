b6fada4d06dfce2852cfbc3c3f2faf016d1beded
Tomas Krizek <tomas.krizek@nic.cz>
2019-08-19 12:36:20 +0200

    fixup! tools/connect: test establishing lots of connections

--- tools/conntest.c
+++ tools/conntest.c
@@ -1,3 +1,20 @@
+/*  conntest.c - tool to test bind before connect for establishing TCP connections
+    Copyright (C) 2019  CZ.NIC, z.s.p.o <knot-resolver@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -10,7 +27,7 @@
 #include <unistd.h>
 #include <assert.h>
 
-#define MAX_CONN_PER_IP 30000
+#define MAX_CONN_PER_IP 60000
 #define NUMBER_OF_IPS 4
 #define CLIENT_IP_STRFORMAT "fd8d:fbca:f789:166::%x"
 #define SERVER_IP_STR "fd8d:fbca:f789:166::cafe"
