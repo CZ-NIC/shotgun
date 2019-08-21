5d3935869ba3c33643504b6d6d34368fbd72b3ae
Tomas Krizek <tomas.krizek@nic.cz>
2019-08-21 15:32:42 +0200

    tools/udpsim: prototype with libuv UDP send/recv

--- tools/udpsim.c
+++ tools/udpsim.c
@@ -17,21 +17,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <uv.h>
+
+#define CLIENT_IP_STR "::1"
+#define SERVER_IP_STR "::1"
+
+
+static void
+_uv_udp_alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
+	buf->base = malloc(suggested_size);
+	buf->len = suggested_size;
+}
+
+static void
+_uv_udp_recv_cb(uv_udp_t* handle, ssize_t nread, const uv_buf_t* buf,
+		const struct sockaddr* addr, unsigned flags) {
+	if (nread == 0 && NULL == addr) {
+		free(buf->base);
+	} else if (nread > 0) {
+		printf("Received: %d\n", nread);
+		for (int i = 0; i < nread; ++i) {
+			printf("%c", buf->base[i]);
+		}
+		printf("\n");
+	}
+}
 
 int main() {
 	uv_loop_t *loop = malloc(sizeof(uv_loop_t));
 	uv_loop_init(loop);
 
-	uv_udp_t udp_socket;
+	uv_udp_t handle;
+
+	uv_buf_t buf;
 
 	struct sockaddr_in6 src;
 	struct sockaddr_in6 dst;
 
+	uv_udp_send_t udp_send_req;
 
-	printf("Now quitting.\n");
+	uv_ip6_addr(CLIENT_IP_STR, 0, &src);
+	uv_ip6_addr(SERVER_IP_STR, 53, &dst);
+
+	uv_udp_init(loop, &handle);
+	uv_udp_bind(&handle, (const struct sockaddr*)&src, 0);
+
+	int addr_len = sizeof(src);
+	uv_udp_getsockname(&handle, (struct sockaddr*)&src, &addr_len);
+
+	printf("srcport: %d\n", ntohs(src.sin6_port));
+
+	unsigned char dnswire[] =
+		// "\x00\x28"  // TCP length
+		"\xfa\x80\x01\x20\x00\x01\x00\x00\x00\x00\x00\x01\x00\x00"
+		"\x01\x00\x01\x00\x00\x29\x10\x00\x00\x00\x00\x00\x00\x0c\x00\x0a"
+		"\x00\x08\x03\x1d\x11\xf5\x53\xda\x33\xdb";
+
+	// start listening for the reply
+	uv_udp_recv_start(&handle, _uv_udp_alloc_cb, _uv_udp_recv_cb);
+	// TODO erro checking
+
+	buf = uv_buf_init(dnswire, sizeof(dnswire));
+	uv_udp_send(&udp_send_req, &handle, &buf, 1, (struct sockaddr*)&dst, NULL);
+
 	uv_run(loop, UV_RUN_DEFAULT);
 
+	uv_close((uv_handle_t*)&handle, NULL);
 	uv_loop_close(loop);
 	free(loop);
 	return 0;
