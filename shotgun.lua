c2d0d6b72f79883179d963a6a9a6d1e32578cafa
Tomas Krizek <tomas.krizek@nic.cz>
2019-09-18 17:38:41 +0200

    shotgun: use dnssim split filter

--- shotgun.lua
+++ shotgun.lua
@@ -1,6 +1,11 @@
 #!/usr/bin/env dnsjit
-local CHANNEL_SIZE = 16384
 local NUM_THREADS = 2
+local TARGET_IP = "fbfb::cafe"
+local TARGET_PORT = 5553
+local BIND_IP_PATTERN = "fbfb::%x"
+local CHANNEL_SIZE = 16384 * 1
+local MAX_CLIENTS_DNSSIM = 100000
+local MAX_BATCH_SIZE = 512
 
 
 local object = require("dnsjit.core.objects")
@@ -33,19 +38,24 @@ if pcap == nil then
 end
 
 
-local function thread_main(thr)
-	-- setup output
-	-- TODO: make dnssim shareable, so it's possible to use thread:pop()?
-	local MAX_UDP_CLIENTS_DNSSIM = 250000
-	local output = require("dnsjit.output.dnssim").new(MAX_UDP_CLIENTS_DNSSIM)
+local function thread_output(thr)
+	local channel = thr:pop()
+	local output = require("dnsjit.output.dnssim").new(thr:pop())
+	local log = require("dnsjit.core.log")
+	local ffi = require("ffi")
+	local running
+
 	output:udp_only()
-	output:target("::1", 53535)
+	output:target(thr:pop(), thr:pop())
 	output:free_after_use(true)
 
-	local MAX_BATCH_SIZE = 32
-	local channel = thr:pop()
-	local unique_id = tostring( {} ):sub(8)
-	local running
+	local outfile = thr:pop()
+	local MAX_BATCH_SIZE = thr:pop()
+
+	local nbind = thr:pop()
+	for i=1,nbind do
+		output:bind(thr:pop())
+	end
 
 	local recv, rctx = output:receive()
 	while true do
@@ -74,38 +84,84 @@ local function thread_main(thr)
 		running = output:run_nowait()
 	end
 
-	-- output results to file
-	output:export("data_"..unique_id..".json")
+	output:export(outfile)
 end
+
+--local function thread_filter(thr)
+--	local split = require("dnsjit.filter.dnssim").new()
+--	local log = require("dnsjit.core.log")
+--	local chann_in = thr:pop()
+--	local chann_out1 = thr:pop()
+--	--local chann_out2 = thr:pop()
+--	local MAX_BATCH_SIZE = chann_in.capacity
+--	split:receiver(chann_out1)
+--	--split:receiver(chann_out2)
+--	local recv, rctx = split:receive()
+--	while true do
+--		local obj
+--		local i = 0
+--
+--		--print("thr filter: "..chann_in:size())
+--		--if chann_in:is_full() == 1 then
+--		--	log.fatal("filter thread can't keep up")
+--		--end
+--
+--		-- read available data from channel
+--		while i < MAX_BATCH_SIZE do
+--			obj = chann_in:try_get()
+--			if obj == nil then break end
+--			recv(rctx, obj)
+--			i = i + 1
+--		end
+--		-- check if channel is still open
+--		if obj == nil and chann_in.closed == 1 then
+--			chann_out1:close()
+--			--chann_out2:close()
+--			break
+--		end
+--	end
+--end
 
 
 -- setup input
-local input = require("dnsjit.input.fpcap").new()
+local input = require("dnsjit.input.mmpcap").new()
 local delay = require("dnsjit.filter.timing").new()
 local layer = require("dnsjit.filter.layer").new()
-local split = require("dnsjit.filter.split").new()  -- TODO ipsplit
+local split = require("dnsjit.filter.dnssim").new()
 local copy = require("dnsjit.filter.copy").new()
 input:open(pcap)
-delay:keep()
-delay:producer(input)
-layer:producer(delay)
+--delay:keep()
+--delay:producer(input)
 
 -- setup threads
 local thread = require("dnsjit.core.thread")
 local channel = require("dnsjit.core.channel")
+local outputs = {}
 local threads = {}
 local channels = {}
-for i = 1,NUM_THREADS+1 do
-	threads[i] = thread.new()
+
+for i=1,NUM_THREADS do
 	channels[i] = channel.new(CHANNEL_SIZE)
-	threads[i]:start(thread_main)
-	threads[i]:push(channels[i])
 	split:receiver(channels[i])
+
+	threads[i] = thread.new()
+	threads[i]:start(thread_output)
+	threads[i]:push(channels[i])
+	threads[i]:push(MAX_CLIENTS_DNSSIM)
+	threads[i]:push(TARGET_IP)
+	threads[i]:push(TARGET_PORT)
+	threads[i]:push("data_"..os.time().."_"..i..".json")
+	threads[i]:push(MAX_BATCH_SIZE)
+	threads[i]:push(1)
+	threads[i]:push(string.format(BIND_IP_PATTERN, i))
 end
 
 copy:layer(object.PAYLOAD)
 copy:layer(object.IP6)
 copy:receiver(split)
+
+layer:producer(input)
+
 
 -- process PCAP
 local prod, pctx = layer:produce()
@@ -116,10 +172,10 @@ while true do
 	recv(rctx, obj)
 end
 
--- close channels and join threads
-for i = 1,NUM_THREADS do
+-- teardown
+for i=1,NUM_THREADS do
 	channels[i]:close()
 end
-for i = 1,NUM_THREADS do
+for i=1,NUM_THREADS do
 	threads[i]:stop()
 end
