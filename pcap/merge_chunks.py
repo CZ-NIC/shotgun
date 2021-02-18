6d3d2695200529ddfb24dc00935c8f210abc2c94
Petr Špaček <pspacek@isc.org>
2021-02-18 16:19:17 +0100

    merge_chunks: utility to merge chunks on-the-fly
    Intended usage is to avoid generating PCAPs which are simple combination
    of "base chunks".
    
    Example:
    When original PCAP is split into 100 chunks with 1 kQPS on average,
    testing full range of 1k to 100k QPS formerly required generating 100
    distincts PCAPs, wasting time and storage.
    With this utility it is enough to generate "base chunks" and create
    arbitrary multiplication on-the-fly.
    
    Why Python?
    - Lua cannot list content of directory, FFI solution would be
      unportable, and adding depedency just for that seems too much.
    - BASH version of this script made me cry when I finished it.
    - Python is already depedency of replay.py and it is used only to
      process mergecap arguments and python process terminates/replaces
      itself once its job is done.

---
+++ pcap/merge_chunks.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import argparse
+import os
+from pathlib import Path
+import random
+import sys
+
+def positive_int(val):
+    i = int(val)
+    if i <= 0:
+        raise ValueError('must be greater than 0')
+    return i
+
+def readable_directory(path):
+    po = Path(path)
+    if not po.is_dir():
+        raise ValueError('must be path to directory')
+    return po
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Merge subset of PCAP chunks on the fly and write result to stdout")
+
+    parser.add_argument('nchunks', type=positive_int,
+                        help='Number of chunks to randomly select from source_dirs and merge')
+    parser.add_argument('source_dirs', nargs='+', type=readable_directory,
+                        help='Paths to directories with PCAP chunks')
+    parser.add_argument('--seed', default=0, type=positive_int,
+                        help='Randomization seed (default: 0)')
+    args = parser.parse_args()
+
+    # reproducible pseudorandomness
+    random.seed(args.seed, version=2)
+
+    pcaps = []
+    for dir_path in args.source_dirs:
+        pcaps.extend(
+            str(path) for path in dir_path.glob('**/*')
+            if path.is_file() or path.is_fifo())
+
+    if args.nchunks > len(pcaps):
+        sys.exit(f'{args.nchunks} chunks requested but only {len(pcaps)} available')
+
+    pcaps.sort()
+    random.shuffle(pcaps)
+    mergecap_args = ['mergecap', '-F', 'pcap', '-w', '-']
+    mergecap_args.extend(pcaps[:args.nchunks])
+
+    sys.stderr.write(f'executing merge command: {mergecap_args}\n')
+    sys.stderr.flush()
+
+    os.execvp('mergecap', mergecap_args)
+
+if __name__ == '__main__':
+    main()
